351e3d1cf7b8ee79cf63a1dbd77aa17f0a26305d
Gennadiy Ukhanov <gukhanov@accesssoftek.com>
2018-08-14 16:01:35 +0500

    Add jsdoc for EnergyCost class

--- src/EnergyCost.js
+++ src/EnergyCost.js
@@ -1,14 +1,10 @@
-
 import ConsumedEnergyList from './utils/ConsumedEnergyList'
 import ScheduleList from './utils/ScheduleList'
 
-/** Class calc optimal device schedule by energy cost and power */
+/** Class for calc optimal device schedule by energy cost and power */
 export default class EnergyCost {
-  static get DAY_HOURS () {
-    return 24
-  }
   /**
-   * Create a energy cost.
+   * Init a energy cost.
    * @param {Array} devices
    * @param {Array} rates
    * @param {Number} maxPower
@@ -16,25 +12,36 @@ export default class EnergyCost {
   constructor ({devices, rates, maxPower}) {
     this.devices = devices
     this.rates = rates
-    this._maxPower = maxPower
+    this.maxPower = maxPower
     this.scheduleList = new ScheduleList()
     this.consumedEnergyList = new ConsumedEnergyList()
   }
+  /**
+   * Start calc schedule.
+   * @param {Object} device
+   * @param {Number} cost
+   */
   calcSchedule () {
     this.devices.forEach(device => {
       let {sequence, cost} = this.getSequence(device)
       if (sequence && cost) {
-        for (let i = sequence; i < sequence + device.duration; i++) {
-          this.scheduleList.add(i, device.id)
-          this.consumedEnergyList.add(device, cost)
+        for (let hour = sequence; hour < sequence + device.duration; hour++) {
+          this.scheduleList.add(device.id, hour)
+          this.consumedEnergyList.add(device.id, device.power, cost)
         }
       }
     })
   }
+  /**
+   * Get best sequence.
+   * @param {Object} device
+   * @return {Object}
+   */
   getSequence (device) {
     let sequence = null
     let cost = Number.POSITIVE_INFINITY
     let offsetHour, startHour, endHour
+    // calc offsets
     switch (device.mode) {
       case 'day':
         offsetHour = 0
@@ -57,30 +64,21 @@ export default class EnergyCost {
         hourCost += this.rates[(i + j + offsetHour) % EnergyCost.DAY_HOURS]
       }
       if (cost > hourCost) {
-        if (this.isEnoughPower(i, device.duration)) {
-          cost = hourCost
-          sequence = (i + offsetHour) % EnergyCost.DAY_HOURS
-        }
-      };
+        cost = hourCost
+        sequence = (i + offsetHour) % EnergyCost.DAY_HOURS
+      }
     }
     return {
       sequence,
       cost
     }
   }
-  isEnoughPower (i, duration) {
-    let isEnoughPower = true
-    for (let j = i; j < i + duration; j++) {
-      let power = 0
-      this.scheduleList.list[i].forEach(deviceId => {
-        power += this.devices.find(device => device.id === deviceId).power
-      })
-      if (power > this._maxPower) {
-        isEnoughPower = false
-      }
-    }
-    return isEnoughPower
-  }
+  /**
+   * Output final schedule.
+   * @param {Mumber} i - index of hour
+   * @param {Number} duration - device duraion
+   * @param {Boolean}
+   */
   output () {
     this.calcSchedule()
     return {
@@ -94,6 +92,9 @@ export default class EnergyCost {
   set devices (devices) {
     this._devices = devices
   }
+  /**
+   * Get sorted devices by duration.
+   */
   get devices () {
     return this._devices.sort((a, b) => {
       return parseInt(a.duration) - parseInt(b.duration)
@@ -102,6 +103,9 @@ export default class EnergyCost {
   set rates (rates) {
     this._rates = rates
   }
+  /**
+   * Get hours rates.
+   */
   get rates () {
     let hours = new Array(EnergyCost.DAY_HOURS)
     this._rates.forEach(rate => {
@@ -116,5 +120,12 @@ export default class EnergyCost {
       }
     })
     return hours
+  }
+  /**
+   * Get day hours.
+   * @static
+   */
+  static get DAY_HOURS () {
+    return 24
   }
 }
